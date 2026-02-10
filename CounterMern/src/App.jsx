06ba174a80bae911ee79ff47dfe921ce5e63b856
Nikita Satish Madke <madkenikita35@gmail.com>
2026-02-10 12:56:36 +0530

    Add Project

---
+++ CounterMern/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Components/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="Mheading">Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ CounterMern/src/Components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h2 className="heading">Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
