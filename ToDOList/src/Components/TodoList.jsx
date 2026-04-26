d2b4cccd7ce28796e2828f0b5462151d86d93aec
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-26 22:37:35 +0530

    Add Changes

--- ToDOList/src/Components/TodoList.jsx
+++ ToDOList/src/Components/TodoList.jsx
@@ -1,53 +1,53 @@
-// import React from "react";
-// import { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
-// const TodoList = () => {
-//   const [todos, setTodos] = useState([]);
-//   const [input, setInput] = useState("");
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
-//   const handleInput = (e) => {
-//     setInput(e.target.value);
-//   };
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
 
-//   const AddTask = () => {
-//     if (input != "") {
-//       setTodos([...todos, input]);
-//       setInput("");
-//     }
-//   };
-//   const canclebtn = (i) => {
-//     setTodos(todos.filter((_, index) => index !== i));
-//   };
+  const AddTask = () => {
+    if (input != "") {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+  const canclebtn = (i) => {
+    setTodos(todos.filter((_, index) => index !== i));
+  };
 
-//   return (
-//     <div>
-//       <h1 className=" heading">To-Do List</h1>
-//       <input
-//         type="text"
-//         value={input}
-//         onChange={handleInput}
-//         placeholder="Enter task"
-//       />
-//       <button className="add-btn" onClick={AddTask}>
-//         Add
-//       </button>
-//       <div className=" ToDODIsplay ">
-//         <ul>
-//           {todos.map((todo, index) => (
-//             <li key={index}>
-//               {todo}
-//               <button className="close-btn" onClick={() => canclebtn(index)}>
-//                 X
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1 className=" heading">To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInput}
+        placeholder="Enter task"
+      />
+      <button className="add-btn" onClick={AddTask}>
+        Add
+      </button>
+      <div className=" ToDODIsplay ">
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button className="close-btn" onClick={() => canclebtn(index)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// export default TodoList;
+export default TodoList;
 
 // // import React, { useState } from "react";
 
