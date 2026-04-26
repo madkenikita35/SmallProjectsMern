3ee3a345152e4f24c00a6f40da5d0895570d6f4c
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-26 22:36:47 +0530

    Add Changes

--- ToDOList/src/Components/TodoList.jsx
+++ ToDOList/src/Components/TodoList.jsx
@@ -1,88 +1,88 @@
-import React from "react";
-import { useState } from "react";
-
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
-
-  const handleInput = (e) => {
-    setInput(e.target.value);
-  };
-
-  const AddTask = () => {
-    if (input != "") {
-      setTodos([...todos, input]);
-      setInput("");
-    }
-  };
-  const canclebtn = (i) => {
-    setTodos(todos.filter((_, index) => index !== i));
-  };
-
-  return (
-    <div>
-      <h1 className="heading">To-Do List</h1>
-      <input
-        type="text"
-        value={input}
-        onChange={handleInput}
-        placeholder="Enter task"
-      />
-      <button className="add-btn" onClick={AddTask}>
-        Add
-      </button>
-      <div className="ToDODIsplay">
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              {todo}
-              <button className="close-btn" onClick={() => canclebtn(index)}>
-                X
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default TodoList;
-
-// import React, { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
 
 // const TodoList = () => {
-//   const [task, setTask] = useState([]);
+//   const [todos, setTodos] = useState([]);
 //   const [input, setInput] = useState("");
 
 //   const handleInput = (e) => {
 //     setInput(e.target.value);
 //   };
 
-//   const addTask = () => {
-//     setTask([...task, input]);
-//     setInput("");
+//   const AddTask = () => {
+//     if (input != "") {
+//       setTodos([...todos, input]);
+//       setInput("");
+//     }
+//   };
+//   const canclebtn = (i) => {
+//     setTodos(todos.filter((_, index) => index !== i));
 //   };
 
 //   return (
 //     <div>
+//       <h1 className=" heading">To-Do List</h1>
 //       <input
 //         type="text"
-//         name="task"
-//         placeholder="Add"
 //         value={input}
 //         onChange={handleInput}
+//         placeholder="Enter task"
 //       />
-//       <button onClick={addTask}>Add Task</button>
-//       <div>
-//         {task.map((task, index) => (
-//           <div key={index}>
-//             <p>{task}</p>
-//           </div>
-//         ))}
+//       <button className="add-btn" onClick={AddTask}>
+//         Add
+//       </button>
+//       <div className=" ToDODIsplay ">
+//         <ul>
+//           {todos.map((todo, index) => (
+//             <li key={index}>
+//               {todo}
+//               <button className="close-btn" onClick={() => canclebtn(index)}>
+//                 X
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
 //       </div>
 //     </div>
 //   );
 // };
 
 // export default TodoList;
+
+// // import React, { useState } from "react";
+
+// // const TodoList = () => {
+// //   const [task, setTask] = useState([]);
+// //   const [input, setInput] = useState("");
+
+// //   const handleInput = (e) => {
+// //     setInput(e.target.value);
+// //   };
+
+// //   const addTask = () => {
+// //     setTask([...task, input]);
+// //     setInput("");
+// //   };
+
+// //   return (
+// //     <div>
+// //       <input
+// //         type="text"
+// //         name="task"
+// //         placeholder="Add"
+// //         value={input}
+// //         onChange={handleInput}
+// //       />
+// //       <button onClick={addTask}>Add Task</button>
+// //       <div>
+// //         {task.map((task, index) => (
+// //           <div key={index}>
+// //             <p>{task}</p>
+// //           </div>
+// //         ))}
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+// // export default TodoList;
